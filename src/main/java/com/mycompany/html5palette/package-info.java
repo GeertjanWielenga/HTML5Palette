6a7ee02aa3a5fdb3db28b175763912a9167ad5a9
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-04 12:18:05 +0200

    data list

--- src/main/java/com/mycompany/html5palette/package-info.java
+++ src/main/java/com/mycompany/html5palette/package-info.java
@@ -1,22 +1,40 @@
 @PaletteItemRegistrations({
-        @PaletteItemRegistration(
-                paletteid = "HTMLPalette",
-                category = "HTML5",
-                itemid = "Audio",
-                icon16 = "com/mycompany/html5palette/icons/audio.png",
-                icon32 = "com/mycompany/html5palette/icons/audio32.png",
-                body = "<audio src=\"somefile.mp3\" controls loop autoplay></audio>",
-                name = "Audio",
-                tooltip = "<audio>"),
-        @PaletteItemRegistration(
-                paletteid = "HTMLPalette",
-                category = "HTML5",
-                itemid = "Video",
-                icon16 = "com/mycompany/html5palette/icons/video.png",
-                icon32 = "com/mycompany/html5palette/icons/video32.png",
-                body = "<video src=\"somefile.mp4\" controls width=\"340\" height=\"210\"></video>",
-                name = "Video",
-                tooltip = "<video>")
+    @PaletteItemRegistration(
+            paletteid = "HTMLPalette",
+            category = "HTML5",
+            itemid = "Audio",
+            icon16 = "com/mycompany/html5palette/icons/audio.png",
+            icon32 = "com/mycompany/html5palette/icons/audio32.png",
+            body = "<audio src=\"somefile.mp3\" controls loop autoplay></audio>",
+            name = "Audio",
+            tooltip = "<audio>"),
+    @PaletteItemRegistration(
+            paletteid = "HTMLPalette",
+            category = "HTML5",
+            itemid = "Video",
+            icon16 = "com/mycompany/html5palette/icons/video.png",
+            icon32 = "com/mycompany/html5palette/icons/video32.png",
+            body = "<video src=\"somefile.mp4\" controls width=\"340\" height=\"210\"></video>",
+            name = "Video",
+            tooltip = "<video>"),
+    @PaletteItemRegistration(
+            paletteid = "HTMLPalette",
+            category = "HTML5",
+            itemid = "Data List",
+            icon16 = "com/mycompany/html5palette/icons/datalist.png",
+            icon32 = "com/mycompany/html5palette/icons/datalist32.png",
+            body = "<label for=\"country_name\">Country: "
+            + "</label>\n<input id=\"country_name\" name=\"country_name\" "
+            + "type=\"text\" list=\"country\" />\n"
+            + "<datalist id=\"country\">\n"
+            + "   <option value=\"Afghanistan\">\n"
+            + "   <option value=\"Albania\">\n"
+            + "   <option value=\"Algeria\">\n"
+            + "   <option value=\"Andorra\">\n"
+            + "   <option value=\"Angola\">\n"
+            + "</datalist>",
+            name = "Data List",
+            tooltip = "<datalist>")
 })
 package com.mycompany.html5palette;
 
