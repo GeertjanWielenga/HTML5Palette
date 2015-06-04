4e9807100f08319b949ba7253988a4b8e213f13a
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-04 11:46:43 +0200

    removing layer and adding video component

---
+++ src/main/java/com/mycompany/html5palette/package-info.java
@@ -0,0 +1,11 @@
+@PaletteItemRegistration( 
+        paletteid = "HTMLPalette", 
+        category = "HTML5", 
+        itemid = "Video", 
+        icon16 = "com/mycompany/html5palette/icons/video.png", 
+        icon32 = "com/mycompany/html5palette/icons/video32.png", 
+        body = "<video src=\"somefile.mp4\" controls width=\"340\" height=\"210\"></video>", 
+        name = "Video", 
+        tooltip = "<video>") 
+package com.mycompany.html5palette;
+import org.netbeans.spi.palette.PaletteItemRegistration;
