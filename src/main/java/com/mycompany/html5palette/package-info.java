7b4e127d0604873f07618cf7d25e7349f92f0ad0
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-04 12:04:10 +0200

    audio

--- src/main/java/com/mycompany/html5palette/package-info.java
+++ src/main/java/com/mycompany/html5palette/package-info.java
@@ -1,11 +1,24 @@
-@PaletteItemRegistration( 
-        paletteid = "HTMLPalette", 
-        category = "HTML5", 
-        itemid = "Video", 
-        icon16 = "com/mycompany/html5palette/icons/video.png", 
-        icon32 = "com/mycompany/html5palette/icons/video32.png", 
-        body = "<video src=\"somefile.mp4\" controls width=\"340\" height=\"210\"></video>", 
-        name = "Video", 
-        tooltip = "<video>") 
+@PaletteItemRegistrations({
+        @PaletteItemRegistration(
+                paletteid = "HTMLPalette",
+                category = "HTML5",
+                itemid = "Audio",
+                icon16 = "com/mycompany/html5palette/icons/audio.png",
+                icon32 = "com/mycompany/html5palette/icons/audio32.png",
+                body = "<audio src=\"somefile.mp3\" controls loop autoplay></audio>",
+                name = "Audio",
+                tooltip = "<audio>"),
+        @PaletteItemRegistration(
+                paletteid = "HTMLPalette",
+                category = "HTML5",
+                itemid = "Video",
+                icon16 = "com/mycompany/html5palette/icons/video.png",
+                icon32 = "com/mycompany/html5palette/icons/video32.png",
+                body = "<video src=\"somefile.mp4\" controls width=\"340\" height=\"210\"></video>",
+                name = "Video",
+                tooltip = "<video>")
+})
 package com.mycompany.html5palette;
+
 import org.netbeans.spi.palette.PaletteItemRegistration;
+import org.netbeans.spi.palette.PaletteItemRegistrations;
