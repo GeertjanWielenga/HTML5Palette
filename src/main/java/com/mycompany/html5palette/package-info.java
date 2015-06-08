ac99693326b0ca7165c6e68252a429da7c364a27
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-06-08 10:17:26 +0200

    support for the new e-mail and range

--- src/main/java/com/mycompany/html5palette/package-info.java
+++ src/main/java/com/mycompany/html5palette/package-info.java
@@ -20,6 +20,50 @@
     @PaletteItemRegistration(
             paletteid = "HTMLPalette",
             category = "HTML5",
+            itemid = "E-Mail",
+            icon16 = "com/mycompany/html5palette/icons/email.png",
+            icon32 = "com/mycompany/html5palette/icons/email32.png",
+            body = "<form>\n"
+            + "<label for=\"email\">What is your e-mail address: </label>\n"
+            + "<input id=\"email\" type=\"email\" required />\n"
+            + "<input type=\"submit\" value=\"Submit\"/> \n"
+            + "</form>",
+            tooltip = "<input name=\"movie\" type=\"email\" required />",
+            name = "E-Mail"),
+    @PaletteItemRegistration(
+            paletteid = "HTMLPalette",
+            category = "HTML5",
+            itemid = "Range",
+            icon16 = "com/mycompany/html5palette/icons/range.png",
+            icon32 = "com/mycompany/html5palette/icons/range32.png",
+            body = "<label for=myRange>Volume:</label>\n"
+            + "<input type=range \n"
+            + "       min=0 \n"
+            + "       max=100 \n"
+            + "       value=50 \n"
+            + "       id=myRange \n"
+            + "       step=1\n"
+            + "       list=volsettings\n"
+            + "       oninput=\"outputUpdate(value)\">\n"
+            + "<datalist id=volsettings>\n"
+            + "    <option>0</option>\n"
+            + "    <option>20</option>\n"
+            + "    <option>40</option>\n"
+            + "    <option>60</option>\n"
+            + "    <option>80</option>\n"
+            + "    <option>100</option>\n"
+            + "</datalist>\n"
+            + "<output for=myRange id=volume>50</output>\n"
+            + "<script>\n"
+            + "    function outputUpdate(vol) {\n"
+            + "        document.querySelector('#volume').value = vol;\n"
+            + "    }\n"
+            + "</script>",
+            tooltip = "<input id=\"myRange\" type=\"range\" />",
+            name = "Range"),
+    @PaletteItemRegistration(
+            paletteid = "HTMLPalette",
+            category = "HTML5",
             itemid = "Required",
             icon16 = "com/mycompany/html5palette/icons/required.png",
             icon32 = "com/mycompany/html5palette/icons/required32.png",
